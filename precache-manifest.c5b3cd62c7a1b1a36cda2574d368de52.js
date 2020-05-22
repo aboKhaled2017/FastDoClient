59a4de6ccf16ffd2f9c1fca02ea40f73a3a99d98
mohamed khaled <mohamed2511995@gmail.com>
2020-05-22 14:44:18 +0300

    Updates

---
+++ precache-manifest.c5b3cd62c7a1b1a36cda2574d368de52.js
@@ -0,0 +1,106 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3ffc8c6db404fb9ad4795584e6e29463",
+    "url": "/FastDoClient/index.html"
+  },
+  {
+    "revision": "187c9347e1e041b6e52b",
+    "url": "/FastDoClient/static/css/main.7d9988b9.chunk.css"
+  },
+  {
+    "revision": "e71c8f66c9b1696d2ab2",
+    "url": "/FastDoClient/static/js/2.c1181dd0.chunk.js"
+  },
+  {
+    "revision": "d09ac505fec0e9130a1d5b85fb3fa6dc",
+    "url": "/FastDoClient/static/js/2.c1181dd0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "187c9347e1e041b6e52b",
+    "url": "/FastDoClient/static/js/main.38cd80ff.chunk.js"
+  },
+  {
+    "revision": "5c3431c6dab63b2bea12",
+    "url": "/FastDoClient/static/js/runtime-main.25db66c5.js"
+  },
+  {
+    "revision": "78502733dff0824c7bfbbaf1d43a223a",
+    "url": "/FastDoClient/static/media/GE_Dinar_Two_Medium.78502733.otf"
+  },
+  {
+    "revision": "4b115e1153a9ea339d6a0bb284cc8ed3",
+    "url": "/FastDoClient/static/media/fa-brands-400.4b115e11.woff2"
+  },
+  {
+    "revision": "80533988ff5fecd5be26557d08ce8237",
+    "url": "/FastDoClient/static/media/fa-brands-400.80533988.svg"
+  },
+  {
+    "revision": "b90365bccdabd68c6c03902b4b141f09",
+    "url": "/FastDoClient/static/media/fa-brands-400.b90365bc.woff"
+  },
+  {
+    "revision": "c39278f7abfc798a241551194f55e29f",
+    "url": "/FastDoClient/static/media/fa-brands-400.c39278f7.ttf"
+  },
+  {
+    "revision": "d9d17590c975aad1be0ddab673f9c769",
+    "url": "/FastDoClient/static/media/fa-brands-400.d9d17590.eot"
+  },
+  {
+    "revision": "414ff5daad323a1c47c5177d4bd29674",
+    "url": "/FastDoClient/static/media/fa-regular-400.414ff5da.eot"
+  },
+  {
+    "revision": "5dd3976cb5d61e2e561f2a46b916f377",
+    "url": "/FastDoClient/static/media/fa-regular-400.5dd3976c.woff"
+  },
+  {
+    "revision": "65779ebcc35604a25c2ba77309c5b8af",
+    "url": "/FastDoClient/static/media/fa-regular-400.65779ebc.woff2"
+  },
+  {
+    "revision": "e7e957c87c454bccaa3bf9fdaa6874f8",
+    "url": "/FastDoClient/static/media/fa-regular-400.e7e957c8.svg"
+  },
+  {
+    "revision": "f6c6f6c8cb7784254ad00056f6fbd74e",
+    "url": "/FastDoClient/static/media/fa-regular-400.f6c6f6c8.ttf"
+  },
+  {
+    "revision": "462806316fea535a6a57651bc2b000b0",
+    "url": "/FastDoClient/static/media/fa-solid-900.46280631.woff2"
+  },
+  {
+    "revision": "61969d433bf265b9717a6c357a1e04e4",
+    "url": "/FastDoClient/static/media/fa-solid-900.61969d43.woff"
+  },
+  {
+    "revision": "82905d8d1c06969df11c8c378e9bdd4c",
+    "url": "/FastDoClient/static/media/fa-solid-900.82905d8d.svg"
+  },
+  {
+    "revision": "b5596f4d339f99e3d69bc41be78db962",
+    "url": "/FastDoClient/static/media/fa-solid-900.b5596f4d.eot"
+  },
+  {
+    "revision": "b70cea0339374107969eb53e5b1f603f",
+    "url": "/FastDoClient/static/media/fa-solid-900.b70cea03.ttf"
+  },
+  {
+    "revision": "2b923fbba5df95cd1ca4bdcd53f18c9d",
+    "url": "/FastDoClient/static/media/homeImage.2b923fbb.jpg"
+  },
+  {
+    "revision": "941d96db7889a5df94f9e5054a59d6dd",
+    "url": "/FastDoClient/static/media/home_company_slider_bg1.941d96db.jpg"
+  },
+  {
+    "revision": "da72dba00019da2148999935b6a35e25",
+    "url": "/FastDoClient/static/media/pharma.da72dba0.jpg"
+  },
+  {
+    "revision": "d72f8cdb94214dbefe2c810bac97dc9c",
+    "url": "/FastDoClient/static/media/storeIcon.d72f8cdb.png"
+  }
+]);
